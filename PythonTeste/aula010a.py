63b11b32bb4afc3646759c14460c6c5fa39aa85f
Hugo Vínicius Conceição Bastos <62792610+HugoDev-Bastos@users.noreply.github.com>
2022-07-04 13:41:50 -0400

    ...

--- PythonTeste/aula010a.py
+++ PythonTeste/aula010a.py
@@ -1,5 +1,17 @@
 # Condições Simples e Compostas #
-nome = str(input('Qual é o seu nome? '))
-if nome == 'Gustavo':
-    print('Que nome lindo você tem')
-print('Bom dia, {}!'.format(nome))
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1+n2) / 2
+
+'''
+if m >= 6.0:
+    print('Sua média foi boa! Parabéns!')
+else:
+    print('Sua média foi ruin! Estude mais!')
+'''
+
+## Condição Simplificada
+
+print("A sua nota foi {:.1f}".format(m))
+print("Parabéns" if m >= 6 else "Estude mais!")
