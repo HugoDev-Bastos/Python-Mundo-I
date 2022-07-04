e9f594245cd9c31074f4d845785800499ea9a138
Hugo Vínicius Conceição Bastos <62792610+HugoDev-Bastos@users.noreply.github.com>
2022-07-04 17:40:17 -0400

    Aumento de salário

--- PythonExércicios/012.py
+++ PythonExércicios/012.py
@@ -2,6 +2,7 @@
 
 pr =  float(input('Qual é o preço do produto? R$'))
 desc = int(input('Qual a % do desconto? '))
+
 novo = pr - (pr * desc /100)
 
 print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custa R${:.2f}'.format(pr, novo))
--- PythonExércicios/034.py
+++ PythonExércicios/034.py
@@ -1,3 +1,13 @@
 # Escrea um programa que pergunte o sálario de um funcionario e calcule o valor do seu aumento
 # PAra salários superiores a R$1.250,00, calcule um aumento de 10%
 #Para os inferiores ou iguais, o aumento é de 15%.
+
+
+salario = float(input('Qual é o salário do funcionário? R$: '))
+
+if salario <= 1250:
+    novo  = salario + (salario * 15 / 100)
+else:
+    novo  = salario + (salario * 10 / 100)
+    
+print('Quem ganhava R${:0.2f} passa a ganhar R${:0.2f} agora.'.format(salario, novo))
