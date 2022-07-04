c22ece39d591bc823f11ea06e570050bf6b7f7f8
Hugo Vínicius Conceição Bastos <62792610+HugoDev-Bastos@users.noreply.github.com>
2022-07-04 14:07:07 -0400

    Exercício 028 concluído

--- PythonExércicios/028.py
+++ PythonExércicios/028.py
@@ -1,3 +1,22 @@
 # Escreva um programa que faça o computador "Pensar" em um número de 0 a 5 e eça para o usuário tenta descpbrir qual foi o número escolhido pelo computador
-
 # O programa deverá escrever na tela se p usuário venceu ou perdeu.
+
+from random import randint
+from time import sleep
+
+print("-=-"*20)
+print(' Vou Pensar em um número entre 0 e 5. Tente adivinhar...')
+print("-=-"*20)
+
+computador = randint(0,5)
+n = int(input('Em que número eu pensei? '))
+print('PROCESSANDO...')
+sleep(3)
+
+if n == computador:
+    print('PARABÉNS, VOCÊ ACERTOU!')
+else:
+    print('VOCÊ ERROU! Pensei no número {} e não no {}!'.format(computador, n))
+
+
+
