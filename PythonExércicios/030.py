f4b12be0d247818074c670f48b6b28f8a5b0802c
Hugo Vínicius Conceição Bastos <62792610+HugoDev-Bastos@users.noreply.github.com>
2022-07-04 16:34:00 -0400

    Exercício 030

--- PythonExércicios/030.py
+++ PythonExércicios/030.py
@@ -1,1 +1,9 @@
 # Cier um programa que leia um número interiro e mostre na tela se ele é PAR ou ÍMPAR
+
+numero = int(input('Me diga um número qualquer: '))
+resultado = numero % 2
+
+if resultado == 0:
+    print ("O resultado foi PAR")
+else:
+    print ("O resultado foi IMPAR")
