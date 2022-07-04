4308b1b8c5ea885b5087dfbeba6adedde7aa6a76
Hugo Vínicius Conceição Bastos <62792610+HugoDev-Bastos@users.noreply.github.com>
2022-07-04 13:28:17 -0400

    Exercicio 28

---
+++ PythonExércicios/028.py
@@ -0,0 +1,3 @@
+# Escreva um programa que faça o computador "Pensar" em um número de 0 a 5 e eça para o usuário tenta descpbrir qual foi o número escolhido pelo computador
+
+# O programa deverá escrever na tela se p usuário venceu ou perdeu.
