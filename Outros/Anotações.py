153d6501efcc47ffaabe973fc245e9f50d5d4405
Hugo Vínicius Conceição Bastos <62792610+HugoDev-Bastos@users.noreply.github.com>
2022-07-05 07:40:28 -0400

    Anotações

--- Outros/Anotações.py
+++ Outros/Anotações.py
@@ -33,3 +33,35 @@
 # IF - Simples
 # IF ELSE - Composta
 ##
+
+
+## Cores no Terminal 
+ 
+## STYLE 
+# none - 0
+# bold - 1
+# underline - 4
+# negativo - 7
+
+## TEXT
+# white - 30
+# red - 31
+# green - 32
+# yelow - 33
+# blue - 34
+# roxo - 35
+# lilaz - 36
+# azul claro - 37
+
+## BACKGROUND
+# white - 40
+# red - 41
+# green - 42
+# yellow - 43
+# blue - 44
+# roxo - 45
+# lilaz - 46
+# azul claro - 47
+
+# \033 [ m
+# \033[ style ; text ; background m
